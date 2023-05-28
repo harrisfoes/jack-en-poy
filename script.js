3dcf0a016705e2f7adb4026af976556bee0bda42
harrisfoes <11287035+harrisfoes@users.noreply.github.com>
2023-05-28 21:27:55 +0800

    progress with card selection

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Hello world!");
 
-function getComputerChoice(){
+function getComputerChoice() {
     let myChoice = ['Rock', 'Paper', 'Scissors'];
     return myChoice[Math.floor(Math.random() * 3)];
 
@@ -8,62 +8,79 @@ function getComputerChoice(){
 
 console.log(getComputerChoice())
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection) {
     let resultMsg = '';
     let result = '';
     let playerChoice = playerSelection.toLowerCase();
     let computerChoice = computerSelection.toLowerCase();
 
-    if(playerChoice === computerChoice){
+    if (playerChoice === computerChoice) {
         result = 'tie';
     }
-    else if (playerChoice == 'rock' && computerChoice == 'scissors'){
-        result ='win';
+    else if (playerChoice == 'rock' && computerChoice == 'scissors') {
+        result = 'win';
     }
-    else if (playerChoice == 'rock' && computerChoice == 'paper'){
+    else if (playerChoice == 'rock' && computerChoice == 'paper') {
         result = 'lose';
     }
-    else if (playerChoice == 'paper' && computerChoice == 'rock'){
-        result ='win';
+    else if (playerChoice == 'paper' && computerChoice == 'rock') {
+        result = 'win';
     }
-    else if (playerChoice == 'paper' && computerChoice == 'scissors'){
+    else if (playerChoice == 'paper' && computerChoice == 'scissors') {
         result = 'lose';
     }
-    else if (playerChoice == 'scissors' && computerChoice == 'paper'){
-        result ='win';
+    else if (playerChoice == 'scissors' && computerChoice == 'paper') {
+        result = 'win';
     }
-    else if (playerChoice == 'scissors' && computerChoice == 'rock'){
+    else if (playerChoice == 'scissors' && computerChoice == 'rock') {
         result = 'lose';
     }
 
-    if(result == 'tie'){
+    if (result == 'tie') {
         resultMsg = `It's a tie between ${playerChoice} and ${computerChoice}!`;
     }
-    else if (result == 'win'){
-        resultMsg =  `You win by choosing ${playerChoice} against ${computerChoice}!`;
+    else if (result == 'win') {
+        resultMsg = `You win by choosing ${playerChoice} against ${computerChoice}!`;
     }
-    else if (result == 'lose'){
-        resultMsg =  `You lose by choosing ${playerChoice} against ${computerChoice}!`;
+    else if (result == 'lose') {
+        resultMsg = `You lose by choosing ${playerChoice} against ${computerChoice}!`;
     }
 
     console.log(resultMsg);
     return result;
 }
 
-function game(){
+function game() {
     let winRecord = 0;
     let loseRecord = 0;
 
-    for( let i = 0; i < 5; i++){
-        //let choice = prompt('Please choose between Rock, Paper and Scissors')
-        let choice = 'rock'
-        //let result = playRound(choice, getComputerChoice())        
 
-        if(result == 'win')
-            winRecord++;
-        else if (result == 'lose')
-            loseRecord++;
-    }
+    let playerChoice = ''
+
+    const jackenpoyButtons = document.querySelectorAll('.card');
+    jackenpoyButtons.forEach((button) => {
+
+        button.addEventListener('click', () => {
+            playerChoice = button.id;
+            console.log(button.id);
+
+            let result = playRound(playerChoice, getComputerChoice())
+            console.log(playerChoice);
+    
+            if (result == 'win') {
+                winRecord++;
+                scorediv = document.querySelector(".scorecard");
+                scorediv.textContent = 'Score:' + winRecord;
+            }
+            else if (result == 'lose')
+                loseRecord++;
+
+        });
+
+    });
+
+
+
 
     if (winRecord > loseRecord)
         console.log(`You win with a score of ${winRecord} against ${loseRecord}`);
